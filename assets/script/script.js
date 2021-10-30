20b0c8877a0454ce5a3766fbe92a56fcb1570971
michell brito <michellbp@msn.com>
2021-10-30 00:41:30 -0400

    added inline-flex, and script to remove grammarly bug

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+// const injectedGrammarlyElm = document.querySelector(
+//   "grammarly-desktop-integration"
+// );
+
+// console.log(injectedGrammarlyElm);
+// function isInPage(node) {
+//   return node === document.body ? false : document.body.contains(node);
+// }
+
+// console.log(isInPage("grammarly-desktop-integration"));
