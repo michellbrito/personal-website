7ac84379d8049fc123cb4a13f31eda052823eb91
michell brito <michellbp@msn.com>
2024-09-07 17:56:45 -0400

    add microsoft clarity

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./reset.css";
 import "./globals.css";
 import "./github-markdown-dark.css";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Michell Brito",
@@ -10,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script id="clarity-script" strategy="afterInteractive" type="text/javascript">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "nzotl3n8ke");
+        `}
+      </Script>
       <body>{children}</body>
     </html>
   );
