9ad4a9a8ea1bbf45fc76eac5cb57c219fb210789
michell brito <michellbp@msn.com>
2024-12-08 14:03:26 -0500

    chore: create allias for components + utils

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function getData(markdownFile) {
+    const fullPath = path.join(process.cwd(), 'content', markdownFile);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    return fileContents;
+}
